cc7172e0f7801856c21c3ea2e787c9aca68f8917
minersail <jkwoo1234@gmail.com>
2018-12-02 23:00:26 -0500

    try to fix javascript 3

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-function switchMap(name)
-{
+function switchMap(name) {
     window.top.postMessage({map: name}, '*'); 
 }
 
-function switchInfo(title, body)
-{
+function switchInfo(title, body) {
 	window.top.postMessage({title: title, body: body}, '*'); 
 }
 
 window.addEventListener('message', function(event) { 
-	console.log(event.origin);
-
-    if (event.data.map)
-    {
-        document.getElementById("campus-map").data = event.data.map;
+    // IMPORTANT: Check the origin of the data! 
+    if (~event.origin.indexOf('https://missybaum.github.io')) { 
+        if (event.data.map) {
+            document.getElementById("campus-map").data = event.data.map;
+        }
+        else {
+            document.getElementById("info-heading").innerHTML = event.data.title;
+            document.getElementById("info").innerHTML = event.data.body;
+        }
     }
-    else
-    {
-        document.getElementById("info-heading").innerHTML = event.data.title;
-        document.getElementById("info").innerHTML = event.data.body;
-    }
-	//console.log(event.origin);
-    // // IMPORTANT: Check the origin of the data! 
-    // if (~event.origin.indexOf('http://yoursite.com')) 
-    // { 
-    //     console.log(event.data); 
-    // } else { 
-    //     console.log("don't hack me!");
-    //     return; 
-    // } 
+    else { 
+        console.log("don't hack me!");
+        return; 
+    } 
 }); 
